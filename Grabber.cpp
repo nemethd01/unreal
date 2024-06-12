d73895ef47a470ea0f94f0002b84144bfb84a63d
nemethd01 <120584904+nemethd01@users.noreply.github.com>
2024-06-12 20:10:03 +0200

    Add files via upload

---
+++ Grabber.cpp
@@ -0,0 +1,175 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Grabber.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	/*UPhysicsHandleComponent* PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	
+	if (PhysicsHandle != nullptr)
+	{
+		UE_LOG(LogTemp, Display, TEXT("Got Physics Handle: %s"), *PhysicsHandle->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No Physics Handle found"));
+	}*/
+	
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	UPhysicsHandleComponent* PhysicsHandle = GetPhysicsHandle();
+	if (PhysicsHandle == nullptr)
+	{
+		return;
+	}
+
+	if (PhysicsHandle->GetGrabbedComponent() != nullptr)
+	{
+		FVector TargetLocation = GetComponentLocation() + GetForwardVector() * HoldDistance;
+		PhysicsHandle->SetTargetLocationAndRotation(TargetLocation, GetComponentRotation());
+	}
+	
+}
+
+/*void UGrabber::PrintDamage(const float& Damage)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Damage: %f"), Damage);
+};
+
+bool UGrabber::HasDamage(float& OutDamage)
+{
+	OutDamage = 5;
+	return true;
+};*/
+
+
+void UGrabber::Grab()
+{
+	/*
+	// Grabber rotation vectors
+FRotator MyRotation = GetComponentRotation();
+FString RotationString = MyRotation.ToCompactString();
+
+UE_LOG(LogTemp, Warning, TEXT("Grabber rotation: %s"), *RotationString);
+
+
+	// World timesec
+float TimeSec = GetWorld()->TimeSeconds;
+UE_LOG(LogTemp, Display, TEXT("Time: %f"), TimeSec);*/
+
+	UPhysicsHandleComponent* PhysicsHandle = GetPhysicsHandle();
+	if (PhysicsHandle == nullptr)
+	{
+		return;
+	}
+	
+	FVector Start = GetComponentLocation();
+	FVector End = Start + GetForwardVector() * MaxGrabDistance;
+	DrawDebugLine(GetWorld(), Start, End, FColor::Red);
+	DrawDebugSphere(GetWorld(), End, 10, 10, FColor::Blue, false, 5); // radius: hány cm
+
+	FCollisionShape Sphere = FCollisionShape::MakeSphere(GrabRadius);
+	FHitResult HitResult;
+	bool HasHit = GetWorld()->SweepSingleByChannel(
+		HitResult,
+		Start, End,
+		FQuat::Identity,
+		ECC_GameTraceChannel2,
+		Sphere
+	);
+
+	if (HasHit)
+	{
+		UPrimitiveComponent* HitComponent = HitResult.GetComponent();
+		HitComponent->WakeAllRigidBodies();
+		PhysicsHandle->GrabComponentAtLocationWithRotation(
+			HitComponent,
+			NAME_None,
+			HitResult.ImpactPoint,
+			GetComponentRotation()
+		);
+	}
+	
+	/* // ez itt a nullptr return vizsgálata előtti rész volt + DrawDebugSphere
+	 if (HasHit)
+	{
+		DrawDebugSphere(GetWorld(), HitResult.Location, 10, 10, FColor::Green, false, 2);
+		DrawDebugSphere(GetWorld(), HitResult.ImpactPoint, 10, 10, FColor::Red, false, 2);
+		// itt pointert kell használni több okból is: Referencia értéknél nem lehet az érték null, itt viszont van hogy nincs hit. A másik, pedig hogy az AActorban többféle típusú objektum is lehet
+		AActor* HitActor = HitResult.GetActor();
+		UE_LOG(LogTemp, Display, TEXT("Hitted actor name: %s"), *HitActor->GetActorNameOrLabel());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Display, TEXT("No actor hit"))
+	}*/
+
+	
+	// reference vs pointer
+	/*float Damage = 0;
+	float& DamageRef = Damage;
+	DamageRef = 5;
+
+	UE_LOG(LogTemp, Display, TEXT("DamageRef: %f, Damage: %f"), DamageRef, Damage);*/
+
+	
+	// PrintDamage(Damage);
+	/*if (HasDamage(Damage))
+	{
+		PrintDamage(Damage);
+	}*/
+}
+
+void UGrabber::Release()
+{
+	// UE_LOG(LogTemp, Display, TEXT("Released grabber"))
+	UPhysicsHandleComponent* PhysicsHandle = GetPhysicsHandle();
+
+	if (PhysicsHandle == nullptr)
+	{
+		return;;
+	}
+	
+	if (PhysicsHandle->GetGrabbedComponent() != nullptr)
+	{
+		PhysicsHandle->GetGrabbedComponent()->WakeAllRigidBodies();
+		PhysicsHandle->ReleaseComponent();
+	}
+	
+	
+}
+
+UPhysicsHandleComponent* UGrabber::GetPhysicsHandle() const
+{
+	UPhysicsHandleComponent* Result = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+
+	if (Result == nullptr)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Grabber requires a UphysicsHandleComponent!"));
+	}
+	
+	return Result;
+}
---
+++ Mover.cpp
@@ -0,0 +1,74 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Mover.h"
+
+#include "Math/UnrealMathUtility.h"
+
+// Sets default values for this component's properties
+UMover::UMover()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UMover::BeginPlay()
+{
+	Super::BeginPlay();
+
+	OriginalLocation = GetOwner()->GetActorLocation();
+	
+}
+
+
+// Called every frame
+void UMover::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	AActor* Owner = GetOwner(); // Get an actor pointer that points at the owner
+
+	// pointer alapok
+	// optiomalizálatlan megoldás kommentekkel:
+	/*float MyFloat = 5;
+	float* YourFloat = &MyFloat;
+	float FloatValue = *YourFloat; // itt a * referenciaként van, nem a pointert jelzi - ez azt csinálja, hogy oda adja az értéket ami azon a lokáción van
+
+	// UE_LOG(LogTemp, Warning, TEXT("YourFloat value: %f"), YourFloat); // Ez nem lesz jó, mert a pointer nem float ezért nem fogja kiírni
+	UE_LOG(LogTemp, Warning, TEXT("YourFloat value: %f"), FloatValue); // Ez nem lesz jó, mert a pointer nem float ezért nem fogja kiírni*/
+
+	// helyes megoldás
+	/*float MyFloat = 5;
+	float* YourFloat = &MyFloat;
+
+	UE_LOG(LogTemp, Warning, TEXT("YourFloat value: %f"), *YourFloat);*/
+
+	
+	// actor owner pointer value
+
+	// FString Name = Owner->GetActorNameOrLabel();
+	//
+	// FVector OwnerLocation = Owner->GetActorLocation();
+	// FString OwnerLocationString = OwnerLocation.ToCompactString();
+	//
+	// UE_LOG(LogTemp, Warning, TEXT("Mover Owner: %s with location: %s"), *Name, *OwnerLocationString);
+	//
+	// UE_LOG(LogTemp, Warning, TEXT("Owner actor pointer address: %u"), Owner);
+
+	if (ShouldMove)
+	{
+		FVector CurrentLocation = GetOwner()->GetActorLocation();
+		FVector TargetLocation = OriginalLocation + MoveOffset;
+		float Speed = FVector::Distance(OriginalLocation, TargetLocation) / MoveTime;
+		
+		FVector NewLocation = FMath::VInterpConstantTo(CurrentLocation, TargetLocation, DeltaTime, Speed);
+
+		GetOwner()->SetActorLocation(NewLocation);
+	}
+}
+
