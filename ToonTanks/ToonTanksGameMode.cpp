83ffda7a18ec62fe79342f45c48c3b87467de357
nemethd01 <120584904+nemethd01@users.noreply.github.com>
2024-10-09 19:59:24 +0200

    Add files via upload

--- ToonTanks/ToonTanksGameMode.cpp
+++ ToonTanks/ToonTanksGameMode.cpp
@@ -16,10 +16,16 @@ void AToonTanksGameMode::ActorDied(AActor* DeadActor)
 		{
 			ToonTanksPlayerController->SetPlayerEnabledState(false);
 		}
+		GameOver(false);
 	}
 	else if (ATower* DestoryedTower = Cast<ATower>(DeadActor))
 	{
 		DestoryedTower->HandleDestruction();
+		--TargetTowers;
+		if (TargetTowers == 0)
+		{
+			GameOver(true);
+		}
 	}
 }
 
@@ -32,6 +38,7 @@ void AToonTanksGameMode::BeginPlay()
 
 void AToonTanksGameMode::HandleGameStart()
 {
+	TargetTowers = GetTargetTowerCount();
 	Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
 	ToonTanksPlayerController = Cast<AToonTanksPlayerController>(UGameplayStatics::GetPlayerController(this, 0));
 
@@ -55,4 +62,11 @@ void AToonTanksGameMode::HandleGameStart()
 			false
 		);
 	}
+}
+
+int32 AToonTanksGameMode::GetTargetTowerCount()
+{
+	TArray<AActor*> Towers;
+	UGameplayStatics::GetAllActorsOfClass(this, ATower::StaticClass(), Towers);
+	return Towers.Num();
 }
--- ToonTanks/ToonTanksGameMode.h
+++ ToonTanks/ToonTanksGameMode.h
@@ -22,6 +22,9 @@ protected:
 
 	UFUNCTION(BlueprintImplementableEvent)
 	void StartGame();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void GameOver(bool BWonGame);
 	
 private:
 	class ATank* Tank;
@@ -30,4 +33,8 @@ private:
 	float StartDelay = 3.f;
 
 	void HandleGameStart();
+
+	int32 TargetTowers = 0;
+	UFUNCTION()
+	int32 GetTargetTowerCount();
 };
